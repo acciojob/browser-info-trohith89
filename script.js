ee983fad474c3e2d32a71e7ce4ff8e8e7e2c1d1b
trohith89 <75958140+trohith89@users.noreply.github.com>
2023-07-02 17:35:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 let browserName = navigator.appName;
 let version = navigator.appVersion;
-document.getElementById("container").innerHTML =  "You are using " + browserName + " version " + version;
+document.getElementById("browser-info").innerHTML =  "You are using " + browserName + " version " + version;
